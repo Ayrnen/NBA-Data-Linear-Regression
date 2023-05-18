a7a2b800c154cd3baf171439fc9c0fd28775df30
Ayrnen <78121902+Ayrnen@users.noreply.github.com>
2023-05-18 18:53:07 -0500

    Add files via upload

--- CSV Practice/Queries/Top10Matchups.sql
+++ CSV Practice/Queries/Top10Matchups.sql
@@ -1,8 +1,22 @@
-SELECT TOP 10 h.team_abbreviation HomeTeam, a.team_abbreviation AwayTeam, COUNT(*) MatchupCount
- FROM HomeGames h
-	JOIN GameInfo g
-	ON h.game_id = g.game_id
-		JOIN AwayGames a
-		ON a.game_id = g.game_id
-GROUP BY h.team_abbreviation, a.team_abbreviation
-ORDER BY COUNT(*) DESC
+SELECT TOP 10 Team1, Team2, COUNT(*) MatchupCount
+
+FROM (
+	SELECT
+		CASE
+			WHEN h.team_abbreviation < a.team_abbreviation THEN h.team_abbreviation
+				ELSE a.team_abbreviation
+				END AS Team1,
+		CASE
+			WHEN h.team_abbreviation > a.team_abbreviation THEN h.team_abbreviation
+				ELSE a.team_abbreviation
+				END AS Team2
+
+	 FROM HomeGames h
+		JOIN GameInfo g
+		ON h.game_id = g.game_id
+			JOIN AwayGames a
+			ON a.game_id = g.game_id
+	 ) AS sub
+
+ GROUP BY Team1, Team2
+ ORDER BY COUNT(*) DESC
